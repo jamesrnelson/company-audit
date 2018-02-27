b9663d30ecd92a02c9a91858e5c3b88d9e9f70b9
JNelson <nelson.jimmy@gmail.com>
2018-02-27 09:54:00 -0700

    iteration 1 employee done

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,19 @@
+# Defines an employee
 class Employee
+
+  attr_reader :employee_id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id.to_i
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
+
+  
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,8 +3,21 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new('5', 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
+
+    assert_instance_of Employee, employee
   end
 
   def test_attributes
+    start_date = Date.new(2015, 1, 1)
+    end_date = Date.new(2018, 1, 1)
+    employee = Employee.new('5', 'Sally Jackson', 'Engineer', start_date, end_date)
+
+    assert_equal 5, employee.employee_id
+    assert_instance_of Integer, employee.employee_id
+    assert_equal 'Sally Jackson', employee.name
+    assert_equal 'Engineer', employee.role
+    assert_equal start_date, employee.start_date
+    assert_equal end_date, employee.end_date
   end
 end
